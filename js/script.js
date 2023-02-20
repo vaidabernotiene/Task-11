86347a57cb8c17145244cf1f9369a998efd211ab
vaidabernotiene <vaidabernotiene@yahoo.com>
2023-02-20 15:45:52 +0200

    fetch data from BASE_URL

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 BASE_URL = "https://melon-potent-period.glitch.me/";
+
+// Fetching URL ---------------------------------------------------
+fetch(BASE_URL + "skills")
+  .then((res) => {
+    if (res.ok) {
+      console.log("GET request succesful");
+    } else {
+      console.log("GET request unsuccesful");
+    }
+    return res;
+  })
+  .then((res) => res.json())
+  .then((result) => {
+   console.log(result)
+  })
+  .catch((err) => console.log(err));
